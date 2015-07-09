843a870bfe7ed605010f3f7256d83ecb87067d3d
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2015-07-09 19:36:11 +0200

    documentation added to the package

--- focus-autosave-mode.el
+++ focus-autosave-mode.el
@@ -21,6 +21,11 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;; `focus-autosave-mode' is a global minor mode saving every modified
+;; file when the Emacs frame loses its focus.
+
 ;;; Code:
 
 ;;;###autoload
